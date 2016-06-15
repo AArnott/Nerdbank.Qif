2788805102465c458d3c9af9fd2ae18611056f6f
David Alpert <dalpert@korewireless.com>
2016-06-15 18:22:16 -0500

    adds a test showing a QifDom instance Configuration is not passed to Import

--- QifApi.Tests/ImportTests.cs
+++ QifApi.Tests/ImportTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using System.IO;
 using NUnit.Framework;
+using QifApi.Config;
 using QifApi.Tests.Helpers;
 
 namespace QifApi.Tests
@@ -28,6 +29,22 @@ namespace QifApi.Tests
             using (var reader = new StreamReader(sample.ToUTF8MemoryStream()))
             {
                 new QifDom().Import(reader);
+            }
+        }
+
+        [Test]
+        public void Can_import_sample_qif_when_current_culture_is_en_CA_with_CustomReadDateFormat()
+        {
+            var sample = ResourceHelpers.ExtractResourceToString("QifApi.Tests.SampleData.sample.qif");
+
+            using (new SpoofCulture(new CultureInfo("en-CA")))
+            using (var reader = new StreamReader(sample.ToUTF8MemoryStream()))
+            {
+                new QifDom(new Configuration
+                    {
+                        ReadDateFormatMode = ReadDateFormatMode.Custom,
+                        CustomReadDateFormat = "M/d/yyyy"
+                    }).Import(reader);
             }
         }
     }
