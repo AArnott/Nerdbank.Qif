4fbf03fabc27dfa21b83d2088c4822aa07403034
Andrew Arnott <andrewarnott@live.com>
2022-04-10 09:27:47 -0600

    Remove `Headers` from public API

--- src/Nerdbank.Qif/Transactions/Headers.cs
+++ src/Nerdbank.Qif/Transactions/Headers.cs
@@ -6,55 +6,55 @@ namespace Nerdbank.Qif;
 /// <summary>
 /// The transaction type headers.
 /// </summary>
-public static class Headers
+internal static class Headers
 {
     /// <summary>
     /// Bank account transactions.
     /// </summary>
-    public const string Bank = "!Type:Bank";
+    internal const string Bank = "!Type:Bank";
 
     /// <summary>
     /// Cash account transactions.
     /// </summary>
-    public const string Cash = "!Type:Cash";
+    internal const string Cash = "!Type:Cash";
 
     /// <summary>
     /// Credit card account transactions.
     /// </summary>
-    public const string CreditCard = "!Type:CCard";
+    internal const string CreditCard = "!Type:CCard";
 
     /// <summary>
     /// Investment account transactions.
     /// </summary>
-    public const string Investment = "!Type:Invst";
+    internal const string Investment = "!Type:Invst";
 
     /// <summary>
     /// Asset account transactions.
     /// </summary>
-    public const string Asset = "!Type:Oth A";
+    internal const string Asset = "!Type:Oth A";
 
     /// <summary>
     /// Liability account transactions.
     /// </summary>
-    public const string Liability = "!Type:Oth L";
+    internal const string Liability = "!Type:Oth L";
 
     /// <summary>
     /// Account list or which account follows.
     /// </summary>
-    public const string AccountList = "!Account";
+    internal const string AccountList = "!Account";
 
     /// <summary>
     /// Category list.
     /// </summary>
-    public const string CategoryList = "!Type:Cat";
+    internal const string CategoryList = "!Type:Cat";
 
     /// <summary>
     /// Class list.
     /// </summary>
-    public const string ClassList = "!Type:Class";
+    internal const string ClassList = "!Type:Class";
 
     /// <summary>
     /// Memorized transaction list.
     /// </summary>
-    public const string MemorizedTransactionList = "!Type:Memorized";
+    internal const string MemorizedTransactionList = "!Type:Memorized";
 }
