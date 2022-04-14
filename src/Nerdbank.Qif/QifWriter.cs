9aaa41598988cf16cce0c241485ff1695bae69eb
Andrew Arnott <andrewarnott@live.com>
2022-04-14 17:50:33 -0600

    Add `QifWriter` with primitive write operations

---
+++ src/Nerdbank.Qif/QifWriter.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Nerdbank.Qif;
+
+/// <summary>
+/// A writer for QIF tokens.
+/// </summary>
+public class QifWriter
+{
+    private readonly TextWriter writer;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QifWriter"/> class.
+    /// </summary>
+    /// <param name="writer">The text writer to emit to.</param>
+    public QifWriter(TextWriter writer)
+    {
+        this.writer = writer ?? throw new ArgumentNullException(nameof(writer));
+    }
+
+    /// <summary>
+    /// Emits a header such as <c>!Type:Bank</c>.
+    /// </summary>
+    /// <param name="name">The header name (e.g. "Type").</param>
+    /// <param name="value">The header value (e.g. "Bank").</param>
+    public void WriteHeader(string name, string? value = null)
+    {
+        this.writer.Write('!');
+        this.writer.Write(name);
+        if (!string.IsNullOrEmpty(value))
+        {
+            this.writer.Write(':');
+            this.writer.Write(value);
+        }
+
+        this.writer.WriteLine();
+    }
+
+    /// <summary>
+    /// Emits a field such as <c>NSome Name</c>.
+    /// </summary>
+    /// <param name="name">The name of the field (e.g. "N").</param>
+    /// <param name="value">The value of the field (e.g. "Checking").</param>
+    public void WriteField(string name, string? value = null)
+    {
+        this.writer.Write(name);
+        this.writer.WriteLine(value);
+    }
+
+    /// <summary>
+    /// Emits a field such as <c>NSome Name</c>.
+    /// </summary>
+    /// <param name="name">The name of the field (e.g. "N").</param>
+    /// <param name="value">The value of the field (e.g. "Checking").</param>
+    public void WriteField(string name, ReadOnlySpan<char> value)
+    {
+        this.writer.Write(name);
+        this.writer.WriteLine(value);
+    }
+}
---
+++ test/Nerdbank.Qif.Tests/QifWriterTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+public class QifWriterTests : TestBase
+{
+    private QifWriter writer;
+    private StringWriter stringWriter;
+
+    public QifWriterTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+        this.stringWriter = new() { NewLine = "\n" };
+        this.writer = new(this.stringWriter);
+    }
+
+    [Fact]
+    public void Ctor_ValidatesArguments()
+    {
+        Assert.Throws<ArgumentNullException>(() => new QifWriter(null!));
+    }
+
+    [Fact]
+    public void WriteHeader_NoValue()
+    {
+        this.writer.WriteHeader("Account");
+        this.AssertWritten("!Account\n");
+    }
+
+    [Fact]
+    public void WriteHeader_WithValue()
+    {
+        this.writer.WriteHeader("Type", "Bank");
+        this.AssertWritten("!Type:Bank\n");
+    }
+
+    [Fact]
+    public void WriteField_NoValue()
+    {
+        this.writer.WriteField("I");
+        this.AssertWritten("I\n");
+    }
+
+    [Fact]
+    public void WriteField_WithValue_String()
+    {
+        this.writer.WriteField("N", "Checking");
+        this.AssertWritten("NChecking\n");
+    }
+
+    [Fact]
+    public void WriteField_WithValue_ReadOnlySpanOfChar()
+    {
+        this.writer.WriteField("N", "Checking".AsSpan());
+        this.AssertWritten("NChecking\n");
+    }
+
+    private void AssertWritten(string qif) => Assert.Equal(qif, this.stringWriter.ToString());
+}
