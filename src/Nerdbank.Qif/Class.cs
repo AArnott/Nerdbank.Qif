2b13bc404b25aa4cea1393e99c64280c9cdabc7c
Andrew Arnott <andrewarnott@live.com>
2022-04-14 15:39:34 -0600

    Add modern `Class` record

---
+++ src/Nerdbank.Qif/Class.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using static Nerdbank.Qif.QifUtilities;
+
+namespace Nerdbank.Qif;
+
+/// <summary>
+/// A class, that may be used to help describe a transaction.
+/// </summary>
+/// <param name="Name">The name of the class.</param>
+public record Class(string Name)
+{
+    /// <summary>
+    /// Gets the description of this class.
+    /// </summary>
+    public string? Description { get; init; }
+
+    /// <summary>
+    /// Deserializes a <see cref="Class"/> from the given <see cref="QifReader"/>.
+    /// </summary>
+    /// <param name="reader">The reader to deserialize from.</param>
+    /// <returns>The deserialized record.</returns>
+    public static Class Load(QifReader reader)
+    {
+        string? name = null;
+        string? description = null;
+        while (reader.TryReadField(out ReadOnlyMemory<char> fieldName, out _))
+        {
+            if (QifUtilities.Equals(FieldNames.Name, fieldName))
+            {
+                name = reader.ReadFieldAsString();
+            }
+            else if (QifUtilities.Equals(FieldNames.Description, fieldName))
+            {
+                description = reader.ReadFieldAsString();
+            }
+        }
+
+        return new(ValueOrThrow(name, FieldNames.Name))
+        {
+            Description = description,
+        };
+    }
+
+    private static class FieldNames
+    {
+        internal const string Name = "N";
+        internal const string Description = "D";
+    }
+}
---
+++ test/Nerdbank.Qif.Tests/ClassTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+public class ClassTests : TestBase
+{
+    public ClassTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    [Fact]
+    public void Load_Simple()
+    {
+        const string qifSource = @"NBonus
+^";
+        using QifReader reader = new(new StringReader(qifSource));
+        Class clazz = Class.Load(reader);
+        Assert.Equal("Bonus", clazz.Name);
+        Assert.Null(clazz.Description);
+    }
+
+    [Fact]
+    public void Load_Exhaustive()
+    {
+        const string qifSource = @"NBonus
+DA bonus
+^";
+        using QifReader reader = new(new StringReader(qifSource));
+        Class clazz = Class.Load(reader);
+        Assert.Equal("Bonus", clazz.Name);
+        Assert.Equal("A bonus", clazz.Description);
+    }
+}
